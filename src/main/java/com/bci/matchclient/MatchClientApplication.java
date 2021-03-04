69331bfaf768a2e0f460b6f0b314c8dafde2bd3e
hassanalam <hassan.alam@bisnode.com>
2021-03-04 19:32:42 +0100

    missing files

---
+++ src/main/java/com/bci/matchclient/MatchClientApplication.java
@@ -0,0 +1,13 @@
+package com.bci.matchclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MatchClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bci/matchclient/MatchClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bci.matchclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
